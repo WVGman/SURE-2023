a4220d41e74c8fb9881af105db830a214952aba3
gpk00003 <122639490+gpk00003@users.noreply.github.com>
2023-06-01 22:24:54 -0400

    added removal of rows with "No Data" in them

--- cleaning obesity data.R
+++ cleaning obesity data.R
@@ -9,11 +9,11 @@ obesityList <- sapply(list.files(full.names=TRUE, path = "./rawData/Obesity from
 obesityList[18] <- NULL #remove link.txt
 
 #check to make sure they have the same fips
-for(x in 1:length(obesityList)){
-  for(y in x:length(obesityList)){
-    print(waldo::compare(sort(obesityList[[x]]$CountyFIPS), sort(obesityList[[y]]$CountyFIPS)))
-  }
-}
+# for(x in 1:length(obesityList)){
+#   for(y in x:length(obesityList)){
+#     print(waldo::compare(sort(obesityList[[x]]$CountyFIPS), sort(obesityList[[y]]$CountyFIPS)))
+#   }
+# }
 #yippee!
 
 #sort each year by the FIPS
@@ -23,17 +23,17 @@ sorted <- lapply(obesityList, arrange, CountyFIPS)
 cols <- paste("X", substr(as.character(2004:2020), 3, nchar(as.character(2004:2020))), sep="")
 
 #create framework for final 
-obesity <- data.frame(cntyfip = numeric(), state = character(), county = character())
+obesity <- data.frame(cntyfip = sorted[[1]]$CountyFIPS, state = sorted[[1]]$State, county = sorted[[1]]$County)
+for(x in 1:length(cols)){
+  obesity[cols[x]] <- sorted[[x]]$Percentage
+}
+
+# write_csv(obesity, "./formattedData/obesityRates.csv")
+
+#remove all rows that have "No Data"
 for(col in cols){
-  obesity[col] = character()
+    obesity <- obesity %>% 
+    filter(obesity[col] != "No Data")
 }
 
-#add the data (i don't remember lol)
-for(row in sorted[[1]]){
-  obesity <- rbind(obesity, data.frame)
-}
-
-years <- 2004:2020
-for(x in 1:length(sorted)){
-  
-}
+write_csv(obesity, "./formattedData/obesityRates.csv")
