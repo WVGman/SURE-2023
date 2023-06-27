1cd996be3bf098d186d66d13870613b55273cab2
gpk00003 <122639490+gpk00003@users.noreply.github.com>
2023-06-27 16:23:29 -0400

    making some plots to show covariate variables, but its hard!
    the population and employment data sucks to plot because there's so much variation within the counties. i made like this weird scatterplot thingy but i'm pretty sure its incredibly confusing

---
+++ lazy population and employment graphs.R
@@ -0,0 +1,75 @@
+library(did)
+library(dplyr)
+library(readr)
+library(Hmisc)
+didTable <- read_csv("./formattedData/DidTable.csv")
+didTable$TREATED <- as.numeric(didTable$TREATED)
+
+didTable$POP <- exp(didTable$LOGPOP)
+didTable$EMPLOY <- exp(didTable$LOGEMPLOY)
+as.data.frame(didTable) -> table
+
+years <- 2010:2019
+popTable <- data.frame(year = numeric(), meanPop = numeric(), medianPop = numeric(), group = character())
+for(year in years){
+  # # tCut <- split(filter(table, YEAR == year), cut2(filter(table, YEAR == 2010)$POP, g=10))
+  # tCut <- split(filter(table, YEAR == year), cut2(filter(table, YEAR == 2010)$POP, c(0, 10000, 100000, 1000000)))
+  # for(t in 1:length(tCut)){
+  #   mean <- mean(tCut[[t]]$POP)
+  #   median <- median(tCut[[t]]$POP)
+  #   popTable <- popTable %>% add_row(year = year, meanPop = mean, medianPop = median, group = names(tCut)[t])
+  # }
+  mean <- mean(filter(table, YEAR == year)$POP)
+  median <- median(filter(table, YEAR == year)$POP)
+  popTable <- popTable %>% add_row(year = year, meanPop = mean, medianPop = median)
+}
+
+popTable$year <- as.character(popTable$year)
+
+ggplot(popTable, aes(x = year, y = meanPop)) + geom_line()
+ggplot(popTable, aes(x = year, y = medianPop, group = group)) + geom_line()
+
+ggplot(popTable, aes(x = year, y = meanPop, group = 1)) + geom_line(linewidth = 2.4, color = "red") + geom_point(
+  fill = "red",
+  size = 5, 
+  pch = 21, # Type of point that allows us to have both color (border) and fill.
+  colour = "#FFFFFF", 
+  stroke = 1 # The width of the border, i.e. stroke.
+) + labs(title = paste(sep="", "Average Population of US Counties, 2010-2019"), 
+         x = "Year", 
+         y = "Average Population") + theme(plot.title = element_text(size=14))
+                                                                    
+ggplot(popTable, aes(x = year, y = medianPop, group = 1)) + geom_line(linewidth = 2.4, color = "lightblue") + geom_point(
+  fill = "lightblue",
+  size = 5, 
+  pch = 21, # Type of point that allows us to have both color (border) and fill.
+  colour = "#FFFFFF", 
+  stroke = 1 # The width of the border, i.e. stroke.
+) + labs(title = paste(sep="", "Median Population of US Counties, 2010-2019"), 
+         x = "Year", 
+         y = "Median Population") + theme(plot.title = element_text(size=14))
+
+#find how many counties went up in population, how many went down
+startandend <- filter(table, YEAR == 2010 | YEAR == 2019)
+up <- 0
+theSame <- 0
+down <- 0
+change <- data.frame(GEO_ID = numeric(), change = numeric(), changePercent = numeric())
+for(i in seq(1, length(startandend$GEO_ID), 2)){
+  x <- startandend$POP[i+1] - startandend$POP[i]
+  y <- (startandend$POP[i+1] - startandend$POP[i])/startandend$POP[i] * 100
+  change <- change %>% add_row(GEO_ID = startandend$GEO_ID[i], change = x, changePercent = y)
+}
+
+change <- arrange(change, change)
+change$num <- 1:length(change$GEO_ID)
+
+ggplot(change, aes(x = num, y=change)) + geom_point()
+
+change <- arrange(change, changePercent)
+change$num <- 1:length(change$GEO_ID)
+ggplot(change, aes(x = num, y=changePercent, color = changePercent)) + geom_point(
+          ) + labs(title = "% Change of Population in Individual US Counties from 2010 to 2019 ", 
+                    x = "Sorted Ranking of Change from Lowest Change to Highest Change", 
+                    y = "Percent Change",
+                   color = "Percent Change of Population") + theme(plot.title = element_text(size=14)) + scale_color_gradient2(low = "darkred", high="darkgreen", mid = "lightyellow", midpoint = 0)
