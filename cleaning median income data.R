2a856b7576061b796d39713df7f9bc200535359a
gpk00003 <122639490+gpk00003@users.noreply.github.com>
2023-07-17 17:22:50 -0400

    cleaned the median income data
    was WAY easier with tidyverse commands and using bing AI to help me get some of the commands right. i'm beginning to see why people like this language now

---
+++ cleaning median income data.R
@@ -0,0 +1,33 @@
+#cleaning median income data
+library(readr)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(ggplot2)
+library(waldo)
+
+incomeList <- sapply(list.files(full.names=TRUE, pattern = "Data.csv?", path = "./rawData/Median Income Census"), read_csv, USE.NAMES = TRUE, skip = 1)
+
+#select only the county IDs and the median income
+#had to split the command in two because for some reason they changed which column means what starting in 2017 and changed the CODES too like what why wouldn't you add them at the end??
+incomeListSimple <- c(lapply(incomeList[1:7], select, c("Geography", `Median income (dollars)!!Estimate!!Households`)),lapply(incomeList[-(1:7)], select, c("Geography", `Estimate!!Median income (dollars)!!HOUSEHOLD INCOME BY RACE AND HISPANIC OR LATINO ORIGIN OF HOUSEHOLDER!!Households`)))
+
+#convert the median income columns (2nd column) to numerics
+incomeListSimple <- lapply(incomeListSimple, mutate_at, 2, as.numeric)
+
+#use inner_join, a function which combines two data frames by merging a column,
+#successfully on every column, so for example, it uses inner_join on columns 1 and 2
+#then on the result of that and column 3, the result of that and column 4, etc
+incomeFinishedTable <- reduce(incomeListSimple, inner_join, by = "Geography")
+names(incomeFinishedTable) <- c("GEO_ID", paste("X", c(10:21), sep=""))
+
+#holy cow that was so much easier than using loops
+
+#drop na values
+sum(is.na(incomeFinishedTable))
+incomeFinishedTable <- drop_na(incomeFinishedTable)
+
+#turn GEO_IDs into numbers
+incomeFinishedTable$GEO_ID <- as.numeric(substring(incomeFinishedTable$GEO_ID, 10))
+
+write_csv(incomeFinishedTable, "./formattedData/medianIncomePerYear2010-2021.csv")
